9d72bcca6f4eee022a683ab3d7c75fdaa3ea4547
jiyoung <amado2u@naver.com>
2016-12-01 09:10:19 +0900

    modify >> Hello.java : 31521 by jiyoung

--- Hello_jsp/src/com/test/hello/Hello.java
+++ Hello_jsp/src/com/test/hello/Hello.java
@@ -4,12 +4,16 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("local Repositiry create Hello_jsp");
-        // 012345:hofix edit by jiyoung
+		// 012345:hofix edit by jiyoung
 		System.out.println("Hotfix edit/modify");
-	    //012345:end
-		]
-	    //0215238:origin/master edit by jiyoung
-			Setyem.out.println("github edit");
+		// 012345:end
+
+		// 0215238:origin/master edit by jiyoung
+		System.out.println("github edit");
+
+		// 31521 : local/master edit by jiyoung
+		System.out.println("local edit 1234");
+		// 31521 : end
 	}
 
 }
